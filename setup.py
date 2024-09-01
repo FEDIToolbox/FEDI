581dec277d9961096dea051d4eba58f54adea28f
h-snoussi <dr.haykel.snoussi@gmail.com>
2024-08-31 20:12:52 -0400

    Adding fedi_dmri_qweights

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
             'fedi_dmri_outliers=FEDI.scripts.fedi_dmri_outliers:main',
             'fedi_dmri_rotate_bvecs=FEDI.scripts.fedi_dmri_rotate_bvecs:main',
             'fedi_dmri_snr=FEDI.scripts.fedi_dmri_snr:main',
+            'fedi_dmri_qweights=FEDI.scripts.fedi_dmri_qweights:main',
         ],
     },
     python_requires='>=3.7',  # Adjust according to your requirements
