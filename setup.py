459762db66ab7828416f87af07fc5c3f45cd24a5
h-snoussi <dr.haykel.snoussi@gmail.com>
2025-01-31 19:08:20 -0500

    correct error in fedi_dmri_reg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fedi',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(include=['FEDI', 'FEDI.*']),
     install_requires=[
         'numpy',
